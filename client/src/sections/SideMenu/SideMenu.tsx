cd617fe841ff1a4d328a828957dc81915e9ba7c1
Alko <alancorcos@hotmail.com>
2023-01-24 13:39:22 -0300

    reactor sideMenu with reducer

--- client/src/sections/SideMenu/SideMenu.tsx
+++ client/src/sections/SideMenu/SideMenu.tsx
@@ -1,158 +1,132 @@
-import { useState, useEffect, useCallback, useRef } from 'react'
-import styles from './sideMenu.module.scss';
+import { useEffect, useCallback, useRef, useReducer } from 'react'
 import { FilterOptionSlider, FilterOptionSwitch, HistoryControl } from '../../components';
 import SimpleBarReact from "simplebar-react";
 import {
     ROTATION_KEY, DEFAULT_ROTATION, BRIGHTNESS_KEY,
     DEFAULT_BRIGHTNESS, CONTRAST_KEY, DEFAULT_CONTRAST,
     EXPOSURE_KEY, DEFAULT_EXPOSURE, GAMMA_KEY, DEFAULT_GAMMA,
-    INVERT_KEY, DEFAULT_INVERT, QUALITY_KEY, DEFAULT_QUALITY
+    INVERT_KEY, QUALITY_KEY, DEFAULT_QUALITY, DEFAULT_FILTER_VALUES
 } from '../../constants';
+import styles from './sideMenu.module.scss';
 import 'simplebar-react/dist/simplebar.min.css';
 
+const filtersReducer = (state: any, action: { type: string; value: number | boolean }) => {
+    switch (action.type) {
+        case action.type:
+            return {
+                ...state,
+                [action.type]: action.value
+            }
+        default:
+            return state;
+    }
+}
+
 const SideMenu = ({ applyFilters, resetFilters }: any) => {
-    const [rotation, setRotation] = useState<number>(DEFAULT_ROTATION)
-    const [brightness, setBrightness] = useState<number>(DEFAULT_BRIGHTNESS)
-    const [contrast, setContrast] = useState<number>(DEFAULT_CONTRAST)
-    const [exposure, setExposure] = useState<number>(DEFAULT_EXPOSURE)
-    const [gamma, setGamma] = useState<number>(DEFAULT_GAMMA)
-    const [invert, setInvert] = useState<boolean>(DEFAULT_INVERT)
-    const [quality, setQuality] = useState<number>(DEFAULT_QUALITY)
-    const filterHistoryRef = useRef([])
+    const filterHistoryRef = useRef([DEFAULT_FILTER_VALUES])
     const filterHistoryIndexRef = useRef(0)
+    const [filtersState, dispatch] = useReducer(filtersReducer, DEFAULT_FILTER_VALUES);
+    const defaultParams = () => { }
 
     useEffect(() => {
         resetAllFilters()
         filterHistoryIndexRef.current = 0
-        filterHistoryRef.current = []
+        filterHistoryRef.current = [DEFAULT_FILTER_VALUES]
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [resetFilters])
 
     useEffect(() => {
         applyFilters(buildParams('', false))
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [invert])
+    }, [filtersState[INVERT_KEY]])
 
-    const resetParam = (paramToReset: any) => {
-        switch (paramToReset) {
-            case ROTATION_KEY: setRotation(DEFAULT_ROTATION);
-                break;
-            case BRIGHTNESS_KEY: setBrightness(DEFAULT_BRIGHTNESS)
-                break;
-            case CONTRAST_KEY: setContrast(DEFAULT_CONTRAST)
-                break;
-            case EXPOSURE_KEY: setExposure(DEFAULT_EXPOSURE)
-                break;
-            case GAMMA_KEY: setGamma(DEFAULT_GAMMA)
-                break;
-            case INVERT_KEY: setInvert(DEFAULT_INVERT)
-                break;
-            case QUALITY_KEY: setQuality(DEFAULT_QUALITY)
-                break;
+
+    const addHistoryRef = () => {
+        const splittedHistory = filterHistoryRef.current.slice(0, filterHistoryIndexRef.current + 1)
+        filterHistoryIndexRef.current = filterHistoryRef.current.length
+        filterHistoryRef.current = [...splittedHistory, filtersState]
+    }
+
+    function removeUnusedParams(params, resetParam) {
+        for (let filter in filtersState) {
+            const isDefaultValue = filtersState[filter] === DEFAULT_FILTER_VALUES[filter]
+            if (!isDefaultValue && filter !== resetParam) {
+                params[filter] = filtersState[filter]
+            }
         }
     }
+
+    const buildParams = (resetParam: string, addToHIstory: boolean) => {
+        const initialValues = { w: 800, h: 600 };
+        const params = { ...initialValues }
+        removeUnusedParams(params, resetParam)
+        addToHIstory && addHistoryRef()
+        return params
+    }
+
+    const loadHistoryState = (newFilterHistoryIndex: number) => {
+        filterHistoryIndexRef.current = newFilterHistoryIndex
+        const historyFilterData = filterHistoryRef.current[newFilterHistoryIndex]
+        const params = {}
+        for (const filter in historyFilterData) {
+            params[filter] = historyFilterData[filter]
+            handleChangeFilter(filter, historyFilterData[filter])
+        }
+        applyFilters(params)
+    }
+
+    const resetParam = (paramToReset: string) => {
+        handleChangeFilter(paramToReset, DEFAULT_FILTER_VALUES[paramToReset])
+    }
+
+    const resetAllFilters = useCallback(() => {
+        for (const filter in filtersState) {
+            handleChangeFilter(filter, DEFAULT_FILTER_VALUES[filter])
+        }
+        applyFilters(defaultParams())
+    }, [applyFilters, filtersState])
+
     const handleOnChangeCheckbox = (paramToReset: string) => {
         resetParam(paramToReset)
         applyFilters(buildParams(paramToReset, true))
     }
 
-    const handleOnChangeFilter = (e: any, filterKey: string) => {
-        switch (filterKey) {
-            case ROTATION_KEY: setRotation(e?.target?.value);
-                break;
-            case BRIGHTNESS_KEY: setBrightness(e?.target?.value)
-                break;
-            case CONTRAST_KEY: setContrast(e?.target?.value)
-                break;
-            case EXPOSURE_KEY: setExposure(e?.target?.value)
-                break;
-            case GAMMA_KEY: setGamma(e?.target?.value)
-                break;
-            case INVERT_KEY: setInvert(e.target.checked)
-                break;
-            case QUALITY_KEY: setQuality(e?.target?.value)
-                break;
-        }
+    const handleChangeFilter = (type: string, value: number | boolean) => {
+        dispatch({ type, value });
     }
 
-    const buildParams = useCallback((resetParam: string, addToHIstory: boolean) => {
-        const paramsObj = {
-            ...(resetParam !== ROTATION_KEY && rotation !== DEFAULT_ROTATION && { [ROTATION_KEY]: rotation }),
-            ...(resetParam !== BRIGHTNESS_KEY && brightness !== DEFAULT_BRIGHTNESS && { [BRIGHTNESS_KEY]: brightness }),
-            ...(resetParam !== CONTRAST_KEY && contrast !== DEFAULT_CONTRAST && { [CONTRAST_KEY]: contrast }),
-            ...(resetParam !== EXPOSURE_KEY && exposure !== DEFAULT_EXPOSURE && { [EXPOSURE_KEY]: exposure }),
-            ...(resetParam !== GAMMA_KEY && gamma !== DEFAULT_GAMMA && { [GAMMA_KEY]: gamma }),
-            ...(resetParam !== INVERT_KEY && invert !== DEFAULT_INVERT && { [INVERT_KEY]: invert }),
-            ...(resetParam !== QUALITY_KEY && quality !== DEFAULT_QUALITY && { [QUALITY_KEY]: quality }),
-            w: 800, h: 600
-        }
-
-        if (addToHIstory) {
-            const splittedHistory = filterHistoryRef.current.slice(0, filterHistoryIndexRef.current + 1)
-            filterHistoryIndexRef.current = filterHistoryRef.current.length
-            filterHistoryRef.current = [...splittedHistory, paramsObj]
-        }
-
-        return paramsObj
-    }, [brightness, contrast, exposure, gamma, invert, quality, rotation])
-
-    const defaultParams = () => { }
-
-    const handleOnChangeCommitted = () => {
-        applyFilters(buildParams('', true))
-    }
-
-    const resetAllFilters = useCallback(() => {
-        setRotation(DEFAULT_ROTATION)
-        setBrightness(DEFAULT_BRIGHTNESS)
-        setContrast(DEFAULT_CONTRAST)
-        setExposure(DEFAULT_EXPOSURE)
-        setGamma(DEFAULT_GAMMA)
-        setInvert(DEFAULT_INVERT)
-        setQuality(DEFAULT_QUALITY)
-        applyFilters(defaultParams())
-    }, [applyFilters])
-
-
-    const loadHistoryState = (newFilterHistoryIndex: number) => {
-        filterHistoryIndexRef.current = newFilterHistoryIndex
-        const paramsObj = filterHistoryRef.current[newFilterHistoryIndex]
-        paramsObj[ROTATION_KEY] ? setRotation(paramsObj[ROTATION_KEY]) : setRotation(DEFAULT_ROTATION)
-        paramsObj[BRIGHTNESS_KEY] ? setBrightness(paramsObj[BRIGHTNESS_KEY]) : setBrightness(DEFAULT_BRIGHTNESS)
-        paramsObj[CONTRAST_KEY] ? setContrast(paramsObj[CONTRAST_KEY]) : setContrast(DEFAULT_CONTRAST)
-        paramsObj[EXPOSURE_KEY] ? setExposure(paramsObj[EXPOSURE_KEY]) : setExposure(DEFAULT_EXPOSURE)
-        paramsObj[GAMMA_KEY] ? setGamma(paramsObj[GAMMA_KEY]) : setGamma(DEFAULT_GAMMA)
-        paramsObj[INVERT_KEY] ? setInvert(paramsObj[INVERT_KEY]) : setInvert(DEFAULT_INVERT)
-        paramsObj[QUALITY_KEY] ? setInvert(paramsObj[QUALITY_KEY]) : setQuality(DEFAULT_QUALITY)
-        applyFilters(paramsObj)
-    }
     const handleOnUndo = () => {
         const newFilterHistoryIndex = filterHistoryIndexRef.current - 1
-        if (newFilterHistoryIndex === -1) {
-            return resetAllFilters()
+        if (newFilterHistoryIndex < 0) {
+            return
         }
         loadHistoryState(newFilterHistoryIndex)
     }
 
     const handleOnRedo = () => {
         const newFilterHistoryIndex = filterHistoryIndexRef.current + 1
-        if (newFilterHistoryIndex >= filterHistoryRef.current.length) {
+        if (newFilterHistoryIndex === filterHistoryRef.current.length) {
             return
         }
         loadHistoryState(newFilterHistoryIndex)
     }
+
+    const handleOnChangeCommitted = () => {
+        applyFilters(buildParams('', true))
+    }
+
     return (
         <div className={styles.sideMenu}>
             <div className={styles.sideMenu__filterOptions}>
                 <SimpleBarReact className={styles.sideMenu__filterBar} >
-                    <FilterOptionSlider defaultValue={DEFAULT_ROTATION} value={rotation} min={0} max={360} paramKey={ROTATION_KEY} title="rotation" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
-                    <FilterOptionSlider defaultValue={DEFAULT_BRIGHTNESS} value={brightness} min={-100} max={100} paramKey={BRIGHTNESS_KEY} title="brightness" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
-                    <FilterOptionSlider defaultValue={DEFAULT_CONTRAST} value={contrast} min={-100} max={100} paramKey={CONTRAST_KEY} title="contrast" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
-                    <FilterOptionSlider defaultValue={DEFAULT_EXPOSURE} value={exposure} min={-100} max={100} paramKey={EXPOSURE_KEY} title="exposure" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
-                    <FilterOptionSlider defaultValue={DEFAULT_GAMMA} value={gamma} min={-100} max={100} paramKey={GAMMA_KEY} title="gamma" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
-                    <FilterOptionSwitch title="invert" paramKey={INVERT_KEY} value={invert} onChangeFilter={handleOnChangeFilter} />
+                    <FilterOptionSlider defaultValue={DEFAULT_ROTATION} value={filtersState[ROTATION_KEY]} min={0} max={360} paramKey={ROTATION_KEY} title="rotation" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(ROTATION_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSlider defaultValue={DEFAULT_BRIGHTNESS} value={filtersState[BRIGHTNESS_KEY]} min={-100} max={100} paramKey={BRIGHTNESS_KEY} title="brightness" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(BRIGHTNESS_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSlider defaultValue={DEFAULT_CONTRAST} value={filtersState[CONTRAST_KEY]} min={-100} max={100} paramKey={CONTRAST_KEY} title="contrast" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(CONTRAST_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSlider defaultValue={DEFAULT_EXPOSURE} value={filtersState[EXPOSURE_KEY]} min={-100} max={100} paramKey={EXPOSURE_KEY} title="exposure" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(EXPOSURE_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSlider defaultValue={DEFAULT_GAMMA} value={filtersState[GAMMA_KEY]} min={-100} max={100} paramKey={GAMMA_KEY} title="gamma" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(GAMMA_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSwitch title="invert" paramKey={INVERT_KEY} value={filtersState[INVERT_KEY]} onChangeFilter={e => handleChangeFilter(INVERT_KEY, e.target.checked)} />
                     <div><h5>Output Quality</h5></div>
-                    <FilterOptionSlider defaultValue={DEFAULT_QUALITY} value={quality} min={0} max={100} paramKey={QUALITY_KEY} title="quality" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={handleOnChangeFilter} onChangeCommited={handleOnChangeCommitted} />
+                    <FilterOptionSlider defaultValue={DEFAULT_QUALITY} value={filtersState[QUALITY_KEY]} min={0} max={100} paramKey={QUALITY_KEY} title="quality" onChangeCheckbox={handleOnChangeCheckbox} onChangeFilter={e => handleChangeFilter(QUALITY_KEY, e.target.value)} onChangeCommited={handleOnChangeCommitted} />
                 </SimpleBarReact>
             </div>
             <HistoryControl handleOnUndo={handleOnUndo} handleOnRedo={handleOnRedo} />
